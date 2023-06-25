4e4d2ee78cd83548efbf6d2613fda79b3a8bb3f4
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-06-25 13:14:38 +0300

    feat: backend project created

---
+++ backend/src/main/java/com/gucardev/financebe/FinanceBeApplication.java
@@ -0,0 +1,13 @@
+package com.gucardev.financebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceBeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FinanceBeApplication.class, args);
+  }
+
+}
---
+++ backend/src/test/java/com/gucardev/financebe/FinanceBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gucardev.financebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceBeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
