2b4b05b786d50ba0d1c430290e6967da09ce7c70
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-06-30 22:10:51 +0300

    feat: card attributes added

--- backend/src/main/java/com/gucardev/financebe/model/card/Card.java
+++ backend/src/main/java/com/gucardev/financebe/model/card/Card.java
@@ -4,15 +4,21 @@ import com.gucardev.financebe.model.Account;
 import com.gucardev.financebe.model.BaseEntity;
 import com.gucardev.financebe.model.User;
 import com.gucardev.financebe.model.transaction.CardTransaction;
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapKeyColumn;
 import jakarta.persistence.OneToMany;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -43,4 +49,10 @@ public class Card extends BaseEntity {
 
   @OneToMany(mappedBy = "card")
   private List<CardTransaction> cardTransactions;
+
+  @ElementCollection(fetch = FetchType.EAGER)
+  @MapKeyColumn(name = "name")
+  @Column(name = "value")
+  @CollectionTable(name = "card_attributes", joinColumns = @JoinColumn(name = "card_id"))
+  private Map<String, String> cardAttributes = new HashMap<>();
 }
