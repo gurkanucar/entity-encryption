0638bf424d8c1997ec02bfc54cd0559265638057
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-06-30 22:11:05 +0300

    feat: user model updated

--- backend/src/main/java/com/gucardev/financebe/model/User.java
+++ backend/src/main/java/com/gucardev/financebe/model/User.java
@@ -4,6 +4,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import java.time.LocalDate;
 import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,10 +28,24 @@ public class User extends BaseEntity {
   private String surname;
 
   @Column(name = "email", length = 30)
+  @Email(message = "Email should be valid")
   private String email;
 
   @Column(name = "identityNumber", length = 11)
+  @Pattern(regexp = "\\d{11}", message = "Identity number should be 11 digits")
   private String identityNumber;
+
+  @Column(name = "phone", length = 20)
+  @Pattern(regexp = "\\d{10,20}", message = "Phone number should be between 10 and 20 digits")
+  private String phone;
+
+  @Column(name = "address", length = 100)
+  @Size(max = 100, message = "Address should not exceed 100 characters")
+  private String address;
+
+  @Column(name = "dateOfBirth")
+  @Past(message = "Date of birth should be in the past")
+  private LocalDate dateOfBirth;
 
   @OneToMany(mappedBy = "user")
   private List<Account> accounts;
