62223b4692a5a941c39bb828728895ebe675f2f8
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-07-02 09:25:27 +0300

    project initialized

---
+++ backend/entity-encryption/src/main/java/com/gucardev/entityencryption/EntityEncryptionApplication.java
@@ -0,0 +1,13 @@
+package com.gucardev.entityencryption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityEncryptionApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EntityEncryptionApplication.class, args);
+  }
+
+}
---
+++ backend/entity-encryption/src/test/java/com/gucardev/entityencryption/EntityEncryptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gucardev.entityencryption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityEncryptionApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
